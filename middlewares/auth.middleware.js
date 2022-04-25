b6a540085162fe4b4a112acc6380259bac4d3a14
theodorusandi <36215621+theodorusandi@users.noreply.github.com>
2022-04-25 13:07:47 -0400

    Update auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ const isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
       next();
   } else {
-      res.status(401).json({ msg: 'You are not authorized to view this resource' });
+      res.status(401).json({ msg: 'You are not authorized to view this resource.' });
   }
 }
 
