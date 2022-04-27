29017067f25d25cedf3856a8b1549131323eab25
theodorusandi <36215621+theodorusandi@users.noreply.github.com>
2022-04-27 17:54:15 -0400

    change sanitation rule for gmail

--- middlewares/requests/forgot-password.request.js
+++ middlewares/requests/forgot-password.request.js
@@ -1,7 +1,7 @@
 const { body } = require("express-validator");
 
 const forgotPasswordRequest = [
-  body("email").isEmail().isLength({ min: 1, max: 255 }).normalizeEmail()
+  body("email").isEmail().isLength({ min: 1, max: 255 }).normalizeEmail({ gmail_remove_dots: false })
 ];
 
 module.exports = forgotPasswordRequest;
