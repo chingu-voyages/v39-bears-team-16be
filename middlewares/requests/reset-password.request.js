1c328a797e5b4550ecc6620da07131d7fb5f3be5
theodorusandi <36215621+theodorusandi@users.noreply.github.com>
2022-04-27 17:43:27 -0400

    change max to 255 chars

--- middlewares/requests/reset-password.request.js
+++ middlewares/requests/reset-password.request.js
@@ -1,8 +1,8 @@
 const { body } = require("express-validator");
 
 const resetPasswordRequest = [
-  body("email").isEmail().isLength({ min: 1, max: 25 }).normalizeEmail(),
-  body("password").isAlphanumeric().isLength({ min: 1, max: 10 }).trim(),
+  body("email").isEmail().isLength({ min: 1, max: 255 }).normalizeEmail(),
+  body("password").isAlphanumeric().isLength({ min: 1, max: 255 }).trim(),
   body("passwordConfirmation").custom((value, { req }) => {
     if (value !== req.body.password) {
       throw new Error('Password confirmation does not match password');
