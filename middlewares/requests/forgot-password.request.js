0a8b33daf684355e7f3c062faf35299c8c1f6978
theodorusandi <36215621+theodorusandi@users.noreply.github.com>
2022-04-27 17:43:27 -0400

    change max to 255 chars

--- middlewares/requests/forgot-password.request.js
+++ middlewares/requests/forgot-password.request.js
@@ -1,7 +1,7 @@
 const { body } = require("express-validator");
 
 const forgotPasswordRequest = [
-  body("email").isEmail().isLength({ min: 1, max: 25 }).normalizeEmail()
+  body("email").isEmail().isLength({ min: 1, max: 255 }).normalizeEmail()
 ];
 
 module.exports = forgotPasswordRequest;
--- middlewares/requests/login-store.request.js
+++ middlewares/requests/login-store.request.js
@@ -1,8 +1,8 @@
 const { body } = require("express-validator");
 
 const loginStoreRequest = [
-  body("email").isEmail().isLength({ min: 1, max: 25 }).normalizeEmail(),
-  body("password").isAlphanumeric().isLength({ min: 1, max: 10 }).trim()
+  body("email").isEmail().isLength({ min: 1, max: 255 }).normalizeEmail(),
+  body("password").isAlphanumeric().isLength({ min: 1, max: 255 }).trim()
 ];
 
 module.exports = loginStoreRequest;
