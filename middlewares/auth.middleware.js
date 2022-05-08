1f82c0ef163d6ec2290e36eb1e6901049c74aaa0
theodorusandi <36215621+theodorusandi@users.noreply.github.com>
2022-05-07 22:51:58 -0400

    return cb on deserialize

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ const isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
+    console.log(req.user);
     res
       .status(401)
       .json({ msg: "You are not authorized to view this resource." });
@@ -12,9 +13,10 @@ const isAdmin = (req, res, next) => {
   if (req.isAuthenticated() && req.user.isAdmin) {
     next();
   } else {
+    console.log(req.user);
     res
       .status(401)
-      .json({ msg: "You are not authorized to view this resource." });
+      .json({ msg: "You are not admin" });
   }
 };
 
