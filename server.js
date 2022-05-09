15427a82c2f31ee5440a13b21434a595bf26e808
theodorusandi <36215621+theodorusandi@users.noreply.github.com>
2022-05-09 16:49:56 -0400

    remove if for csrf

--- server.js
+++ server.js
@@ -45,12 +45,14 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(csrf());
-app.use((req, res, next) => {
-  res.locals.csrfToken = req.csrfToken();
-  res.locals.user = req.user;
-  next();
-});
+
+// app.use((req, res, next) => {
+//   res.locals.csrfToken = req.csrfToken();
+//   res.locals.user = req.user;
+//   next();
+// });
 
 app.use(helmet());
 app.use(compression());
