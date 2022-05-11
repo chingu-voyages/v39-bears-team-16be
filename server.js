93f9ba386e56f56c853d5c5292ba110d12d8c265
theodorusandi <36215621+theodorusandi@users.noreply.github.com>
2022-05-11 17:22:50 -0400

    add development mode

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: /^http:\/\/localhost:3000/,
+    origin: [/^http:\/\/localhost:3000/,/^https:\/\/bears-team-16be.herokuapp.com/],
     credentials: true,
   })
 );
