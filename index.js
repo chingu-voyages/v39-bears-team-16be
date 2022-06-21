1b446a3fcb0abef04a6194cec4da04cbe3a29f18
andi gunawan <theodorusandi.g@gmail.com>
2022-06-20 23:18:57 -0400

    add update delete

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ require('dotenv').config();
 
 const { MongoClient } = require('mongodb');
 
+const app = require('./app');
 const userDao = require('./app/dao/user.dao');
 const passwordResetTokenDao = require('./app/dao/password-reset-token.dao');
-const cohortDao = require('./app/dao/cohort.dao');
-const classDao = require('./app/dao/class.dao');
-const app = require('./app');
+const planDao = require('./app/dao/plan.dao');
 
 const port = process.env.PORT || 5000;
 const client = new MongoClient(process.env.MONGODB_URI);
@@ -17,8 +16,7 @@ const main = async () => {
 
     await userDao.initialize(client);
     await passwordResetTokenDao.initialize(client);
-    await cohortDao.initialize(client);
-    await classDao.initialize(client);
+    await planDao.initialize(client);
 
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
--- routes/web.routes.js
+++ routes/web.routes.js
@@ -11,6 +11,7 @@ const {
   forgotPasswordController,
   resetPasswordController,
   githubAuthController,
+  planController,
 } = require('./index');
 
 const router = express.Router();
@@ -28,7 +29,10 @@ router
 router.route('/auth/github').get(githubAuthController.index);
 router.route('/auth/github/callback').get(githubAuthController.store);
 // resources
-
+router.route('/plans/create').post(planController.store);
+router.route('/plans/:planId').get(planController.show);
+router.route('plans/:planId').put(planController.update);
+router.route('/plans/:planId').delete(planController.destroy);
 // csrf token
 router.route('/fetchCsrfToken').get((req, res) => {
   res.status(200).send({ csrfToken: req.csrfToken() });
