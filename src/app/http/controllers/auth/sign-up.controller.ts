5a81e3c18a47e700cdf722dde745246831935ac5
RDiMag <87457132+RDiMag@users.noreply.github.com>
2022-09-09 21:45:41 -0400

    Converted controllers and middlewares to typescript

---
+++ src/app/http/controllers/auth/sign-up.controller.ts
@@ -0,0 +1,30 @@
+const { userDao, hashPassword } = require('./index');
+
+function SignUpControlle() {
+  this.store = async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const { hash, salt } = hashPassword(password);
+
+    try {
+      await userDao.create({
+        name,
+        email,
+        hash,
+        salt,
+      });
+      return res
+        .status(200)
+        .send({ success: true, message: 'A user has been created.' });
+    } catch (err) {
+      return next(err);
+    }
+  };
+}
+
+const signUpController = new SignUpControlle();
+
+Object.freeze(signUpController);
+
+module.exports = signUpController;
+
+export {};
---
+++ src/app/http/controllers/enrollment.controller.ts
@@ -0,0 +1,22 @@
+const { userDao } = require('./auth');
+
+function EnrollmentController() {
+  this.index = async (req, res, next) => {
+    const { email } = req.user;
+    try {
+      const plans = await userDao.allPlans(email);
+      return res.status(200).json({ plans });
+    } catch (err) {
+      console.error(err);
+      return next(err);
+    }
+  };
+}
+
+const enrollmentController = new EnrollmentController();
+
+Object.freeze(enrollmentController);
+
+module.exports = enrollmentController;
+
+export {};
