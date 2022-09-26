1c670074c941150010cfebcf16e6de9c737568f2
Theodorus Andi G <theodorusandi.g@Gmail.com>
2022-09-25 21:03:09 -0400

    Revert "54 create progress model (#59)"
    This reverts commit 5a8043ecdf66ed7bdde9ca0277052f423b26f6e5.

---
+++ app/http/controllers/index.js
@@ -0,0 +1,13 @@
+const classController = require('./class.controller');
+const classworkController = require('./classwork.controller');
+const enrollmentController = require('./enrollment.controller');
+const likeController = require('./like.controller');
+const planController = require('./plan.controller');
+
+module.exports = {
+  classController,
+  classworkController,
+  enrollmentController,
+  likeController,
+  planController,
+};
